6a459f09d42bc780e7f897817138cf89b23cf532
jc <juan.cruz.business@gmail.com>
2024-09-23 20:32:11 -0500

    issues with setup py for graspnet

--- models/graspnet/pointnet2/setup.py
+++ models/graspnet/pointnet2/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import glob
+import os
 
 _ext_src_root = "_ext_src"
 _ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
@@ -15,9 +16,14 @@ setup(
             name='pointnet2._ext',
             sources=_ext_sources,
             extra_compile_args={
-                "cxx": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
-                "nvcc": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
+                "cxx": ["-O2", "-I{}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), _ext_src_root, "include"))],
+                "nvcc": [
+                    "-O2", 
+                    "-I{}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), _ext_src_root, "include")),
+                    "-I{}".format(os.path.join(os.environ.get('CUDA_HOME', '/usr/local/cuda'), 'include'))
+                ],
             },
+            include_dirs=[os.path.join(os.environ.get('CUDA_HOME', '/usr/local/cuda'), 'include')]
         )
     ],
     cmdclass={
