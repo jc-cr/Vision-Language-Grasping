55509dd794a9ca3b5471c25cc72f86ed26d825fc
jc <juan.cruz.business@gmail.com>
2024-09-23 20:28:07 -0500

    issues with setup py for graspnet

--- models/graspnet/pointnet2/setup.py
+++ models/graspnet/pointnet2/setup.py
@@ -1,15 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# 
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-from torch.utils.cpp_extension import CUDA_HOME
 import glob
-import os
-
-ROOT = os.path.dirname(os.path.abspath(__file__))
 
 _ext_src_root = "_ext_src"
 _ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
@@ -24,14 +15,9 @@ setup(
             name='pointnet2._ext',
             sources=_ext_sources,
             extra_compile_args={
-                "cxx": ["-O2", "-I{}".format("{}/{}/include".format(ROOT, _ext_src_root))],
-                "nvcc": [
-                    "-O2", 
-                    "-I{}".format("{}/{}/include".format(ROOT, _ext_src_root)),
-                    "-I{}".format(os.path.join(CUDA_HOME, 'include'))
-                ],
+                "cxx": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
+                "nvcc": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
             },
-            include_dirs=[os.path.join(CUDA_HOME, 'include')]
         )
     ],
     cmdclass={
