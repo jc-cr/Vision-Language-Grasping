7a80371f9775c2c24c863fc2cd22286f52f340af
jc <juan.cruz.business@gmail.com>
2024-09-24 02:25:52 -0500

    fixing cant find ext

--- models/graspnet/pointnet2/setup.py
+++ models/graspnet/pointnet2/setup.py
@@ -1,36 +1,30 @@
-import os
-import glob
+# pointnet2/setup.py
+
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
+import glob
+import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
-print("ROOT:", ROOT)
 
 _ext_src_root = "_ext_src"
-_ext_sources = glob.glob(os.path.join(ROOT, _ext_src_root, "src", "*.cpp")) + \
-               glob.glob(os.path.join(ROOT, _ext_src_root, "src", "*.cu"))
-_ext_headers = glob.glob(os.path.join(ROOT, _ext_src_root, "include", "*.h"))
+_ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
+    "{}/src/*.cu".format(_ext_src_root)
+)
+_ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
 
-print("_ext_sources:", _ext_sources)
-print("_ext_headers:", _ext_headers)
-
-def get_extensions():
-    ext_modules = [
+setup(
+    name='pointnet2',
+    ext_modules=[
         CUDAExtension(
             name='pointnet2._ext',
             sources=_ext_sources,
             extra_compile_args={
-                "cxx": ["-O2", "-I{}".format(os.path.join(ROOT, _ext_src_root, "include"))],
-                "nvcc": ["-O2", "-I{}".format(os.path.join(ROOT, _ext_src_root, "include"))],
+                "cxx": ["-O2", "-I{}".format("{}/{}/include".format(ROOT, _ext_src_root))],
+                "nvcc": ["-O2", "-I{}".format("{}/{}/include".format(ROOT, _ext_src_root))],
             },
         )
-    ]
-    return ext_modules
-
-setup(
-    name='pointnet2',
-    ext_modules=get_extensions(),
+    ],
     cmdclass={
-        'build_ext': BuildExtension.with_options(no_python_abi_suffix=True)
+        'build_ext': BuildExtension
     }
 )
